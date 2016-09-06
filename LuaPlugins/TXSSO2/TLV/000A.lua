dc77c6a064729631a63457e91ab809343bcf59fa
文pig4210 <pig4210>
2016-09-06 09:33:33 +0800

    修正一个小Bug

--- LuaPlugins/TXSSO2/TLV/000A.lua
+++ LuaPlugins/TXSSO2/TLV/000A.lua
@@ -6,6 +6,6 @@ dissectors.tlv[0x000A] = function( buf, pkg, root, t, off, size )
   return dissectors.add( t, buf, 0,
     ">wTlvVer W",
     ">wErrorCode W",
-    ">ErrorMsg wxline_string",
+    ">ErrorMsg wxline_string"
     );
 end
